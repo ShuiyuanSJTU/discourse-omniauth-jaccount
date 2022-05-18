88ef00b7b6b3cd35032fa7568395f6ce2f20c9f8
Jiajun Du <jiajun_du@outlook.com>
2022-05-18 11:13:14 +0000

    feat: clean plugin_store when user delete

--- plugin.rb
+++ plugin.rb
@@ -2,8 +2,11 @@
 # about: login with jAccount
 # version: 0.0.1
 # authors: Rong Cai(feynixs)
+# url: https://github.com/ShuiyuanSJTU/discourse-omniauth-jaccount
 
 gem 'omniauth-jaccount', '0.1.3'
+PLUGIN_NAME ||= 'auth-jaccount'.freeze
+enabled_site_setting :jaccount_auth_enabled
 
 class JAccountAuthenticator < ::Auth::Authenticator
 
@@ -12,7 +15,7 @@ class JAccountAuthenticator < ::Auth::Authenticator
   end
 
   def enabled?
-    true
+    SiteSetting.jaccount_auth_enabled
   end
 
   def after_authenticate(auth_token)
@@ -26,13 +29,13 @@ class JAccountAuthenticator < ::Auth::Authenticator
     ja_uid = email if ja_uid&.strip == ""
 
     # Plugin specific data storage
-    current_info = ::PluginStore.get("auth-jaccount", "ja_uid_#{ja_uid}")
+    current_info = ::PluginStore.get(PLUGIN_NAME, "ja_uid_#{ja_uid}")
 
     # Check if the user is trying to connect an existing account
     unless current_info
       existing_user = User.joins(:user_emails).find_by(user_emails: { email: email })
       if existing_user
-        ::PluginStore.set("auth-jaccount", "ja_uid_#{ja_uid}", { user_id: existing_user.id })
+        ::PluginStore.set(PLUGIN_NAME, "ja_uid_#{ja_uid}", { user_id: existing_user.id })
         result.user = existing_user
       end
     else
@@ -52,7 +55,7 @@ class JAccountAuthenticator < ::Auth::Authenticator
 
   def after_create_account(user, auth)
     data = auth[:extra_data]
-    ::PluginStore.set("auth-jaccount", "ja_uid_#{data[:jaccount_uid]}", { user_id: user.id })
+    ::PluginStore.set(PLUGIN_NAME, "ja_uid_#{data[:jaccount_uid]}", { user_id: user.id })
   end
 
   def register_middleware(omniauth)
@@ -84,3 +87,12 @@ end
 
 auth_provider title: 'with jAccount',
               authenticator: JAccountAuthenticator.new
+
+after_initialize do
+  on(:user_destroyed) do |user|
+    row = PluginStoreRow.find_by(plugin_name: PLUGIN_NAME, value: { user_id: user.id }.to_json)
+    if row
+      row.destroy
+    end
+  end
+end
