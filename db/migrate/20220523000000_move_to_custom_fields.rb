5e6c6958599c4d6dd1b7a0629e33e743c4419a20
Jiajun Du <jiajun_du@outlook.com>
2022-05-23 03:27:00 +0000

    fix: wrong name

--- db/migrate/20220523000000_move_to_custom_fields.rb
+++ db/migrate/20220523000000_move_to_custom_fields.rb
@@ -1,14 +1,14 @@
 class MoveToCustomFields < ActiveRecord::Migration[7.0]
   def change
-    PLUGIN_NAME = "auth-jaccount"
-    rows = PluginStoreRow.where(plugin_name: PLUGIN_NAME)
+    plugin_name = "auth-jaccount"
+    rows = PluginStoreRow.where(plugin_name: plugin_name)
     rows.each do |row|
       ja_uid = row.key
       ja_uid = ja_uid[9, ja_uid.length]
       user_id = JSON.parse(row.value).user_id
       user = User.find_by(id: user_id)
       if user
-        user.custom_fields[PLUGIN_NAME] = ja_uid
+        user.custom_fields[plugin_name] = ja_uid
         user.save_custom_fields
       end
       row.destroy
